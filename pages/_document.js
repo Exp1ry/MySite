dc4a04ed61a17ff8ef505c429a5f44c16fb96a58
Zaid <fpszaid@gmail.com>
2022-10-31 13:20:56 +0100

    f

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Roboto+Mono:wght@200&display=swap"
             rel="stylesheet"
           ></link>
+          <title>Full Stack Web Developer</title>
+          <meta
+            name="description"
+            content="Full Stack Web Developer specializing in the MERN Stack, which includes ReactJS, ExpressJS, NodeJS and MongoDB. Blockchain & Web3 enthusiast"
+          ></meta>
         </Head>
         <body>
           <Main />
