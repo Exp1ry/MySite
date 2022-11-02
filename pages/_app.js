cb84985e82378e3e1c440f664c5f3a41106b6651
Zaid <fpszaid@gmail.com>
2022-11-02 12:11:24 +0100

    f

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import "../styles/globals.css"
 import "bootstrap/dist/css/bootstrap.css"
 import Script from "next/script"
-
+import { Analytics } from "@vercel/analytics"
 import NavBarComp from "../components/Navbar"
 import Footer from "../components/Footer"
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <NavBarComp /> <Component {...pageProps} /> <Footer />
+      <NavBarComp /> <Component {...pageProps} />
+      <Analytics />
+      <Footer />
       <Script src="https://kit.fontawesome.com/f260f67609.js"></Script>
     </>
   )
